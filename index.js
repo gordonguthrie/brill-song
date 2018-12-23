b6eeb1a19da1a493a7beec0c3d6ca6010706f574
Gordon Guthrie <gordon@hypernumbers.com>
2018-12-24 00:42:49 +0100

    Mixed release:
    * implement structs for all data components
    * full create/read/updatge cycle for data objects

--- index.js
+++ index.js
@@ -1,55 +1,113 @@
+var fs = require('fs');
+const classFactory = require('./struct');
+
+//
+// Define constants
+//
+// We represent a song by various data structures
+// these are 1-level objects which are represented on disk
+// in json
+//
+// These constants are arrays of field definitions
+//
+const titleFields = ["title"];
+const songwritersFields = ["songwriters"];
+
+//
+// generic version function
+//
 exports.version = function() {
-    console.log("This is version 1 of the brill data package");
+    return "This is version 1 of brill-song";
 }
 
-var fs = require('fs');
-
-// open creates a song object serialised into the passed in directory
-// wraps it in a closure and returns an api to it
-exports.open = function(dir) {
+//
+// the function open creates a song object serialised into the passed in
+// directory. It wraps it in a closure and returns an api to it
+//
+exports.open = function(dir, songwriters) {
     var song = {};
     var api = {};
+
+    // define some classes to contain information
+    let Title = classFactory.makeStruct("title", titleFields);
+    let Songwriters = classFactory.makeStruct("songwriters", songwritersFields);
     
     // define data elements
     song.is_valid = false;
     song.directory = dir;
+    song.title = new Title("title");
+    song.songwriters = new Songwriters("songwriters");
 
+    // set the data we have
+    song.songwriters.set("songwriters", songwriters);
+    
     // define api functions
     var dumpfn = function () {
-	console.log("dumping the current song");
-	console.log("directory is " + song.directory);
+	console.log("dumping the current song****************");
+	console.log("directory   : " + song.directory);
+	song.title.dump();
+	song.songwriters.dump();
+	console.log("dump end********************************");
     };
 
-    var dirtyfn = function (dirty) {
-	console.log(dirty + " is dirty");
+    var getFn = function(component, field) {
+	var comp = song[component];
+	return comp.get(field);
+    }
+
+    var setFn = function(val, component, field) {
+	var comp = song[component];
+	comp.set(field, val);
+	write(component);
     }
 
     // bind the closures to the return object
     api.dump = dumpfn;
-    api.mark_dirty = dirtyfn;
+    api.get = getFn;
+    api.set = setFn;
     
     //
     // define internal functions
     //
 
-    var does_file_exist = function () {
-	false;
+    var write = function (component) {
+	contents = song[component];
+	write_file(component, contents.get_json());
+    }
+	
+    var does_file_exist = function (file) {
+	var path = song.directory + "/" + file + ".brill";
+ 	try {
+	    fs.accessSync(path, fs.constants.F_OK);
+	    return true;
+	} catch(err) {
+	    return false;
+	}
     };
 
     var write_file = function(filename, contents) {
 	var path = song.directory + "/" + filename + ".brill";
 	fs.writeFile(path, contents, (err) => {
 	    if(err) {
-		alert("error writing file + err.msg")
+		console.log("error writing file" + err.msg)
 	    }});
     };
-    
+
+    var read_file = function(filename) {
+	var path = song.directory + "/" + filename + ".brill";
+	return fs.readFileSync(path, 'utf-8', (err, data) => {
+	    if (err) {
+		console.log("file " + path + " does not exist");
+	    }
+	});
+    }
+		    
     var get_title = function () {
 	var segments = song.directory.split('/');
 	var length = segments.length;
-	var obj = new Object();
-	obj.title =  segments[length - 1];
-	return JSON.stringify(obj);
+	var title = segments[length - 1];
+	song.title.set("title", title);
+	return song.title.get_json();
     };
     
     var create_title = function () {
@@ -57,22 +115,30 @@ exports.open = function(dir) {
 	write_file("title", title);
     }
 
-    create_songwriters = function () {
-	var songwriters;
-	if (settings.hash('songwriters')) {
-	    songwriter = settings.get('songwriters');
-	} else {
-	    songwriters = ""
-	}
-	var obj = new Object();
-	obj.songwriter = songwriter;
-	write_file("songwriters", JSON.stringify(obj));
+    var create_songwriters = function () {
+	write_file("songwriters", song.songwriters.get_json());
     }
     
     var create_new = function () {
-	create_songwriters();
 	create_title();
+	create_songwriters();
+	song.is_valid = true;
     };
+
+    var read_title = function () {
+	var json = read_file("title");
+	song.title.load_json(json);
+    }
+
+    var read_songwriters = function () {
+	var json = read_file("songwriters");
+	song.songwriters.load_json(json);
+    }
+    
+    var read_song = function () {
+	read_title();
+	read_songwriters();
+    }
     
     //
     // the main execution of the function
@@ -81,14 +147,28 @@ exports.open = function(dir) {
     // first we check if the song is valid
     // there are two initial criteria
     // * if the song directory doesn't exist we can create it and are good to go
-    // * if the song directory contains a song.brill file
+    // * if the song directory contains a title.brill file
     //   we assume it contains a song and try and load it file by file
 
-    if (!does_file_exist(song.directory)) {
+    if (!does_file_exist("title")) {
 	create_new();
     } else {
-	console.log("song exists lets read it");
+	read_song();;
     }
     
     return api;
+}
+
+exports.sandbox = function () {
+    let Banjo = classFactory.makeStruct("banjo", ["bobby", "jumpers"]);
+    var myclass = new Banjo("ergo");
+    var myobj = new Object();
+    myobj.bobby = "rando";
+    myobj.jumpers = "brando";
+    var json = JSON.stringify(myobj);
+    console.log("json is " + json);
+    myclass.dump();
+    myclass.load_json_into_class(json);
+    myclass.dump();
+    console.log(myclass.get_json());
 }
---
+++ struct.js
@@ -0,0 +1,61 @@
+// This is a factory for creating data structures
+
+exports.makeStruct = function (className, fields) {
+    if (Array.isArray(fields)) {
+
+	return class {
+            constructor(name) {
+		this.name = className;
+		this.fields = fields;
+		this.data = new Object();
+		for (var i = 0; i < this.fields.length; i++) {
+		    var fieldname = this.fields[i];
+		    this.data[fieldname] = "";
+		}
+	    };
+
+	    dump() {
+		console.log("class name is: " + this.name);
+		for (var i = 0; i < this.fields.length; i++) {
+		    var fieldname = this.fields[i];
+		    console.log(fieldname + " is " + this.data[fieldname]);
+		}
+	    }
+
+	    load_json(json) {
+		var jsonobj = JSON.parse(json);
+		for (var key in jsonobj) {
+		    if (jsonobj.hasOwnProperty(key)) {
+			this.set(key, jsonobj[key]);
+		    }
+		}
+	    }
+
+	    get_json() {
+		return JSON.stringify(this.data);
+	    }
+
+	    // this silently discards fields not defined in the structure
+	    set(fieldname, value) {
+		for (var i = 0; i < this.fields.length; i++) {
+		    if (this.fields[i] == fieldname) {
+			this.data[fieldname] = value;
+		    }
+		}
+	    }
+
+	    get(fieldname) {
+		for (var i = 0; i < this.fields.length; i++) {
+		    if (this.fields[i] == fieldname) {
+			return this.data[fieldname];
+		    }
+		}
+		return "";
+	    }
+		
+	}
+    } else {
+	throw("makeStruct takes an array of field names as an argument");
+    }
+}
+
