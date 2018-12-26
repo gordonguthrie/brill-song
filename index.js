6bfacf6dc5d1cb92af27febc064ecdb86d99649d
Gordon Guthrie <gordon@hypernumbers.com>
2018-12-27 00:50:17 +0100

    start adding the compiler

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 const classFactory = require('./struct');
+//const compiler = require('./compiler');
 
 //
 // Define constants
@@ -12,6 +13,7 @@ const classFactory = require('./struct');
 //
 const titleFields = ["title"];
 const songwritersFields = ["songwriters"];
+const timingFields = ["bpm", "beats_to_the_bar"];
 
 //
 // generic version function
@@ -31,13 +33,15 @@ exports.open = function(dir, songwriters) {
     // define some classes to contain information
     let Title = classFactory.makeStruct("title", titleFields);
     let Songwriters = classFactory.makeStruct("songwriters", songwritersFields);
+    let Timing = classFactory.makeStruct("timing", timingFields);
     
     // define data elements
     song.is_valid = false;
     song.directory = dir;
     song.title = new Title("title");
     song.songwriters = new Songwriters("songwriters");
-
+    song.timing = new Timing("timing");
+    
     // set the data we have
     song.songwriters.set("songwriters", songwriters);
     
@@ -61,20 +65,28 @@ exports.open = function(dir, songwriters) {
 	write(component);
     }
 
+    var compileFn = function() {
+	console.log("compile the song into ruby");
+	var contents = "make contents";
+	var title = make_ruby(song.title.get("title"));
+	write_file("/src/" + title, contents, ".rb");
+    }
+    
     // bind the closures to the return object
     api.dump = dumpfn;
     api.get = getFn;
     api.set = setFn;
+    api.compile = compileFn;
     
     //
     // define internal functions
     //
 
     var write = function (component) {
-	contents = song[component];
-	write_file(component, contents.get_json());
-    }
-	
+	var contents = song[component];
+	write_file(component, contents.get_json(), ".brill");
+    };
+
     var does_file_exist = function (file) {
 	var path = song.directory + "/" + file + ".brill";
  	try {
@@ -82,14 +94,14 @@ exports.open = function(dir, songwriters) {
 	    return true;
 	} catch(err) {
 	    return false;
-	}
+	};
     };
 
-    var write_file = function(filename, contents) {
-	var path = song.directory + "/" + filename + ".brill";
+    var write_file = function(filename, contents, filetype) {
+	var path = song.directory + "/" + filename + filetype;
 	fs.writeFile(path, contents, (err) => {
 	    if(err) {
-		console.log("error writing file" + err.msg)
+		console.log("error writing file: " + path + " giving " + err.msg)
 	    }});
     };
 
@@ -100,8 +112,14 @@ exports.open = function(dir, songwriters) {
 		console.log("file " + path + " does not exist");
 	    }
 	});
-    }
-		    
+    };
+
+    var make_ruby = function (string) {
+	return string.toLowerCase()
+	    .replace(/ /g, "_")
+	    .replace(/[^a-zA-Z0-9_]/g, "");
+    };
+    
     var get_title = function () {
 	var segments = song.directory.split('/');
 	var length = segments.length;
@@ -112,16 +130,28 @@ exports.open = function(dir, songwriters) {
     
     var create_title = function () {
 	var title = get_title();
-	write_file("title", title);
+	write_file("title", title, ".brill");
     }
 
     var create_songwriters = function () {
-	write_file("songwriters", song.songwriters.get_json());
+	var contents = song.songwriters.get_json();
+	write_file("songwriters", contents, ".brill");
+    }
+
+    var create_timing = function () {
+	write_file("timing", song.timing.get_json(), ".brill");
+    }
+
+    var create_src_dir = function () {
+	fs.mkdirSync(song.directory + "/src");
     }
     
     var create_new = function () {
 	create_title();
 	create_songwriters();
+	create_timing();
+	// now make the compile output directory
+	create_src_dir();
 	song.is_valid = true;
     };
 
@@ -134,10 +164,16 @@ exports.open = function(dir, songwriters) {
 	var json = read_file("songwriters");
 	song.songwriters.load_json(json);
     }
+
+    var read_timing = function () {
+	var json = read_file("timing");
+	song.timing.load_json(json);
+    }
     
     var read_song = function () {
 	read_title();
 	read_songwriters();
+	read_timing();
     }
     
     //
@@ -153,8 +189,8 @@ exports.open = function(dir, songwriters) {
     if (!does_file_exist("title")) {
 	create_new();
     } else {
-	read_song();;
-    }
+	read_song();
+    };
     
     return api;
 }
